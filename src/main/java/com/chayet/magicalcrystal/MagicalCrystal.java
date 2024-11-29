8d625cf2b6d6505f42ec9aa28c2e8f3fc62531f5
Leo <leo6@DDongs-MBP2019-16inch-i9-1T.local>
2024-11-29 19:39:07 +0800

    Initial commit

---
+++ src/main/java/com/chayet/magicalcrystal/MagicalCrystal.java
@@ -0,0 +1,16 @@
+package com.chayet.magicalcrystal;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicalCrystal implements ModInitializer {
+	public static final String MOD_ID = "magicalcrystal";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/chayet/magicalcrystal/MagicalCrystalClient.java
@@ -0,0 +1,10 @@
+package com.chayet.magicalcrystal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicalCrystalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
