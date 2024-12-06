6045ed32ef236b96e2cbca079ee8b20ded5e8c0a
chayet <chayet1431@gmail.com>
2024-12-06 23:59:40 +0800

    Basic items and blocks and itemGroups Update 2

--- src/main/java/com/chayet/magicalcrystal/items/ModItems.java
+++ src/main/java/com/chayet/magicalcrystal/items/ModItems.java
@@ -1,9 +1,7 @@
 package com.chayet.magicalcrystal.items;
 
 import com.chayet.magicalcrystal.MagicalCrystal;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
