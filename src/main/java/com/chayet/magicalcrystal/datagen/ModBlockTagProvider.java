8e3186c8024b7e04c3e4bd478752e00c4739c6d4
chayet <chayet1431@gmail.com>
2025-11-20 23:54:09 +0800

    texture added

--- src/main/java/com/chayet/magicalcrystal/datagen/ModBlockTagProvider.java
+++ src/main/java/com/chayet/magicalcrystal/datagen/ModBlockTagProvider.java
@@ -35,6 +35,17 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         getOrCreateTagBuilder(BlockTags.FENCES).add(ModBlocks.CHAOS_FENCE);
         getOrCreateTagBuilder(BlockTags.FENCE_GATES).add(ModBlocks.CHAOS_FENCE_GATE);
 
+        getOrCreateTagBuilder(BlockTags.LOGS).add(ModBlocks.CHAOS_LOG).add(ModBlocks.STRIPPED_CHAOS_LOG);
+        getOrCreateTagBuilder(BlockTags.PLANKS).add(ModBlocks.CHAOS_PLANKS);
+        getOrCreateTagBuilder(BlockTags.WOODEN_BUTTONS).add(ModBlocks.CHAOS_BUTTON);
+        getOrCreateTagBuilder(BlockTags.WOODEN_DOORS).add(ModBlocks.CHAOS_DOOR);;
+        getOrCreateTagBuilder(BlockTags.WOODEN_TRAPDOORS).add(ModBlocks.CHAOS_TRAPDOOR);
+        getOrCreateTagBuilder(BlockTags.WOODEN_SLABS).add(ModBlocks.CHAOS_SLAB);
+        getOrCreateTagBuilder(BlockTags.WOODEN_PRESSURE_PLATES).add(ModBlocks.CHAOS_PRESSURE_PLATE);
+        getOrCreateTagBuilder(BlockTags.WOODEN_FENCES).add(ModBlocks.CHAOS_FENCE);
+        getOrCreateTagBuilder(BlockTags.WOODEN_STAIRS).add(ModBlocks.CHAOS_STAIRS);
+
+
         getOrCreateTagBuilder(ModTags.Blocks.NEED_MAGICAL_CRYSTAL_TOOL)
                 .addTag(BlockTags.NEEDS_DIAMOND_TOOL);
     }
